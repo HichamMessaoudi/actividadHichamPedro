39c9ff1ffc78884854cc892e61c585ff4e91bcf6
hicham <hicham@PC02.mshome.net>
2017-03-17 14:35:37 +0100

    Primera subida

---
+++ programaHicham/src/programahicham/ProgramaHicham.java
@@ -0,0 +1,12 @@
+package programahicham;
+
+/**
+ * @author hicham
+ */
+public class ProgramaHicham {
+
+    public static void main(String[] args) {
+        System.out.println("La probabilidad de que el barça gane la champions es de 15%");  
+    }
+    
+}
