f115f2f3085569867d83265d0a23ec1b683c1a8b
hicham <hicham@PC02.mshome.net>
2017-03-22 13:17:03 +0100

    Archivo editado tras hcaer una nueva rama

--- programaHicham/src/programahicham/ProgramaHicham.java
+++ programaHicham/src/programahicham/ProgramaHicham.java
@@ -6,7 +6,8 @@ package programahicham;
 public class ProgramaHicham {
 
     public static void main(String[] args) {
-        System.out.println("La probabilidad de que el barça gane la champions es de 50%");  
+        System.out.println("La probabilidad de que el barça gane la champions es de 50%"); 
+        System.out.println("¿De verdad no queremos un partido de la Champions Barça-Real Madrid?");
     }
     
 }
